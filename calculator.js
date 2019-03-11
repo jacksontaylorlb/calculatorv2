fc0c485f89dcc3891c7dde86292e2628cf7ff0b0
jacksontaylorlb <46732062+jacksontaylorlb@users.noreply.github.com>
2019-03-11 11:53:23 -0700

    new files

---
+++ calculator.js
@@ -0,0 +1,60 @@
+var express = require("express");
+var app = express();
+var PORT = 8080;
+
+app.get('/:operator/:num1/:num2', function(req,res){
+
+    var operator = req.params.operator;
+    var num1 = parseInt(req.params.num1);
+    var num2 = parseInt(req.params.num2);
+    var result;
+
+    switch(operator){
+        case "add":
+            result = add(num1, num2);
+            break;
+
+        case "subtract":
+            result = subtract(num1, num2);
+            break;
+
+        case "multiply":
+            result = multiply(num1, num2);
+            break;
+
+        case "divide":
+            result = divide(num1, num2);
+            break;
+
+        default:
+            result = "Sorry, please enter a valid operator."
+    }
+
+    function add(a, b){
+        return a + b
+    }
+
+    function subtract(a, b){
+        return a - b
+    }
+
+    function multiply(a, b){
+        return a * b
+    }
+
+    function divide(a, b){
+        return a / b
+    }
+
+    res.json(result);
+});
+
+app.get("/", function(req, res){
+    res.send("hello, please type your math question into the address bar after the URL" +
+        ". example: /add/1/2, other operators include: subtract, multiply, and divide ")
+});
+
+//listener
+app.listen(PORT, function(){
+    console.log("App listening on PORT:" + PORT);
+});
